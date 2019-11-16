e7f4d8005dc2aa029a8b6ba800d0cf95c48cb0bd
Jacob Strong <ja.ha.strong@gmail.com>
2019-11-15 20:39:20 -0700

    fix stuff

--- src/router.js
+++ src/router.js
@@ -12,39 +12,39 @@ export default new Router({
       component: () => import(/* webpackChunkName: "StoreIndex" */ './views/store/StoreIndex.vue'),
       children: [
         {
-          path: '/', name: 'Store Landing', component: () => import(/* webpackChunkName: "StoreLanding" */ './views/store/StoreLanding.vue')
+          path: '/', name: 'Store Landing', component: () => import(/* webpackChunkName: "StoreLanding" */ './views/store/StoreIndex.vue')
         }
       ]
     },
-    // {
-    //   path: '/',
-    //   name: 'Login',
-    //   component: () => import(/* webpackChunkName: "Login" */ './views/Login.vue')
-    // },
-    // {
-    //   path: '/signup/',
-    //   name: 'Signup',
-    //   component: () => import(/* webpackChunkName: "Signup" */ './views/Signup.vue')
-    // },
-    // {
-    //   path: '/emailconfirmation/:id',
-    //   name: 'Email Confirmation',
-    //   component: () => import(/* webpackChunkName: "EmailConfirmation" */ './views/EmailConfirmation.vue')
-    // },
-    // {
-    //   path: '/cancelemailconfirmation/:id',
-    //   name: 'Cancel EmailVerification',
-    //   component: () => import(/* webpackChunkName: "CancelEmailVerification" */ './views/CancelEmailConfirmation.vue')
-    // },
-    // {
-    //   path: '/passwordreset/',
-    //   name: 'Password Reset',
-    //   component: () => import(/* webpackChunkName: "CancelEmailVerification" */ './views/PasswordReset.vue')
-    // },
-    // {
-    //   path: '/passwordreset/:id',
-    //   name: 'Password Set',
-    //   component: () => import(/* webpackChunkName: "CancelEmailVerification" */ './views/PasswordReset.vue')
-    // }
+    {
+      path: '/',
+      name: 'Login',
+      component: () => import(/* webpackChunkName: "Login" */ './views/Login.vue')
+    },
+    {
+      path: '/signup/',
+      name: 'Signup',
+      component: () => import(/* webpackChunkName: "Signup" */ './views/Signup.vue')
+    },
+    {
+      path: '/emailconfirmation/:id',
+      name: 'Email Confirmation',
+      component: () => import(/* webpackChunkName: "EmailConfirmation" */ './views/EmailConfirmation.vue')
+    },
+    {
+      path: '/cancelemailconfirmation/:id',
+      name: 'Cancel EmailVerification',
+      component: () => import(/* webpackChunkName: "CancelEmailVerification" */ './views/CancelEmailConfirmation.vue')
+    },
+    {
+      path: '/passwordreset/',
+      name: 'Password Reset',
+      component: () => import(/* webpackChunkName: "CancelEmailVerification" */ './views/PasswordReset.vue')
+    },
+    {
+      path: '/passwordreset/:id',
+      name: 'Password Set',
+      component: () => import(/* webpackChunkName: "CancelEmailVerification" */ './views/PasswordReset.vue')
+    }
   ]
 })
